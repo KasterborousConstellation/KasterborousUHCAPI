a1943d351c4cec1c9cd92eb56098e94e5429772a
Kasterbourous <paul.andrieu28@gmail.com>
2022-07-24 14:14:51 +0200

    Second commit

--- src/fr/supercomete/head/GameUtils/Scenarios/ScenarioCompatibility.java
+++ src/fr/supercomete/head/GameUtils/Scenarios/ScenarioCompatibility.java
@@ -2,6 +2,13 @@ package fr.supercomete.head.GameUtils.Scenarios;
 import fr.supercomete.head.GameUtils.GameMode.ModeModifier.GameModeModifier;
 import fr.supercomete.head.GameUtils.GameMode.Modes.Mode;
 public class ScenarioCompatibility {
+    /*
+    This class is used to specify the compatibility of scenarios.
+    You can add a scenario that can only be added to some specified GameMode with a whitelist.
+    Or you can add a scenario that can be used by all the GameMode with some exceptions with a blacklist.
+    You can add a scenario that work only with some GameModeModifiers like Groupable. That would mean that the scenario can only be used by GameMode
+    that use a group system.
+     */
     public static ScenarioCompatibility allModes= new ScenarioCompatibility(ScenarioCompatibilityType.BlackList,new Mode[0]);
     private ScenarioCompatibilityType type;
     private Mode[] modes;
