48b95ba92aa36178737ebb08daa3bf2ad060e60a
Kasterbourous <paul.andrieu28@gmail.com>
2022-11-05 19:37:03 +0100

    V1.01

--- src/fr/supercomete/enums/Gstate.java
+++ src/fr/supercomete/enums/Gstate.java
@@ -1,5 +1,16 @@
 package fr.supercomete.enums;
 
 public enum Gstate {
+    /*
+    Enumération
+    Description: Décrit les différentes étapes de la partie
+
+    Waiting: Etat avant le démarrage de la partie
+    Starting: Durant la téléportation des joueurs
+    Playing: Etape après la téléportation et avant le début du jour
+    Day: Durant le jour
+    Night: Durant la nuit
+    Finish: Pendant la fin de la partie après l'annonce de la victoire.
+     */
 	Waiting,Starting,Playing,Day,Night,Finish
 }
